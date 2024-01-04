87ebe5c87bc137672beabdc46e01cf6d9467bc4d
bswlkh0310 <hhhello0507@gmail.com>
2024-01-04 15:09:11 +0900

    chore : annotation ui test

--- alimo/Targets/Alimo/Sources/Application/AlimoApp.swift
+++ alimo/Targets/Alimo/Sources/Application/AlimoApp.swift
@@ -5,7 +5,10 @@ struct AlimoApp: App {
     var body: some Scene {
         WindowGroup {
             // if you want to test ui, then you can remove annotation
-             UITestView()
+            // UITestView()
+            
+            
+            // add something!
         }
     }
 }
