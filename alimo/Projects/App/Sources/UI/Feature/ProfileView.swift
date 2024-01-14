b370ae73fb7a613584aed4e2c69461fd8ad165d8
jyw28 <roy03280328@gmail.com>
2024-01-15 03:14:30 +0900

    feat: Add catagory

--- alimo/Projects/App/Sources/UI/Feature/ProfileView.swift
+++ alimo/Projects/App/Sources/UI/Feature/ProfileView.swift
@@ -12,14 +12,14 @@ struct ProfileView: View {
     
     @State var someToggle: Bool = true
     
+    let dummyCategory: [String] = ["1학년", "바인드", "사운드체크", "사운드체크", "도서부", "교장선생님이 알립니다"]
+    
     var body: some View {
         NavigationStack {
             ScrollView(showsIndicators: false) {
                 VStack(spacing: 10) {
-                    Circle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(Color.gray100)
-                        .padding(.top, 40)
+                    Image(Asset.profileImage)
+                        .padding(.top, 100)
                     
                     Text("김가영")
                         .font(Font.body)
@@ -30,19 +30,16 @@ struct ProfileView: View {
                         .foregroundStyle(Color.gray500)
                         .underline()
                     
-                    HStack {
-                        ForEach(1...3, id: \.self) { _ in
-                            Text("사운드체크")
-                                .font(Font.custom(Pretendard.medium.rawValue, size: 12))
-                                .foregroundStyle(Color.gray500)
-                                .padding(.horizontal, 10)
-                                .padding(.vertical, 5)
-                                .background(Color.gray100)
-                                .clipShape(Capsule())
-                        }
-                        
-                    }
-                    .padding(.top, 10)
+                    FlowLayout(mode: .scrollable,
+                                   items: dummyCategory) {
+                          Text($0)
+                            .font(Font.custom(Pretendard.medium.rawValue, size: 12))
+                            .foregroundStyle(Color.gray500)
+                            .padding(.horizontal, 10)
+                            .padding(.vertical, 5)
+                            .background(Color.gray100)
+                            .clipShape(Capsule())
+                        }.padding()
                     
                     Toggle("알림 설정", isOn: $someToggle)
                         .tint(Color.main500)
@@ -52,26 +49,39 @@ struct ProfileView: View {
                 }
                 .background(Color.main50)
                 
-                Color.gray100
-                    .frame(height: 10)
-                
-                VStack(alignment: .leading, spacing: 20) {
+                VStack(alignment: .leading, spacing: 0) {
                     NavigationLink {
-                        // 개인정보 이용 약관 뷰
+                        // 개인정보 이용 약관
                     } label: {
-                        Text("개인정보 이용 약관")
-                            .font(Font.bodyLight)
-                            .foregroundStyle(.black)
-                            .padding(.top, 20)
+                        Rectangle()
+                            .frame(height: 60)
+                            .foregroundStyle(Color.main50)
+                            .overlay {
+                                HStack {
+                                    Text("개인정보 이용 약관")
+                                        .font(Font.bodyLight)
+                                        .foregroundStyle(.black)
+                                    Spacer()
+                                }
+                                .padding(.leading, 20)
+                            }
                     }
                     
                     NavigationLink {
                         // 서비스 정책
                     } label: {
-                        Text("서비스 정책")
-                            .font(Font.bodyLight)
-                            .foregroundStyle(.black)
-                            .padding(.top, 20)
+                        Rectangle()
+                            .frame(height: 60)
+                            .foregroundStyle(Color.main50)
+                            .overlay {
+                                HStack {
+                                    Text("서비스 정책")
+                                        .font(Font.bodyLight)
+                                        .foregroundStyle(.black)
+                                    Spacer()
+                                }
+                                .padding(.leading, 20)
+                            }
                     }
                     
                     HStack {
@@ -83,7 +93,8 @@ struct ProfileView: View {
                             .font(Font.custom(Pretendard.medium.rawValue, size: 16))
                             .foregroundStyle(Color.gray500)
                     }
-                    .padding(.top, 20)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 20)
                     
                     Divider()
                     
@@ -93,16 +104,16 @@ struct ProfileView: View {
                         Text("로그아웃")
                             .font(Font.custom(Pretendard.medium.rawValue, size: 16))
                             .foregroundStyle(Color.red500)
-                            .padding(.bottom, 10)
                     }
+                    .padding(.leading, 20)
+                    .padding(.vertical, 20)
+                    
                 }
-                .padding(.horizontal, 20)
-                
-                Color.gray100
-                    .frame(height: 200)
+                .background(Color.main50)
                 
             }
-            .background(Color.main50)
+            .ignoresSafeArea()
+            .background(Color.gray100)
         }
         .onAppear {
             UIScrollView.appearance().bounces = false
@@ -110,5 +121,116 @@ struct ProfileView: View {
         .onDisappear {
             UIScrollView.appearance().bounces = true
         }
+    }
+}
+
+public let flowLayoutDefaultItemSpacing: CGFloat = 4
+
+public struct FlowLayout<RefreshBinding, Data, ItemView: View>: View {
+  let mode: Mode
+  @Binding var binding: RefreshBinding
+  let items: [Data]
+  let itemSpacing: CGFloat
+  @ViewBuilder let viewMapping: (Data) -> ItemView
+
+  @State private var totalHeight: CGFloat
+
+  public init(mode: Mode,
+              binding: Binding<RefreshBinding>,
+              items: [Data],
+              itemSpacing: CGFloat = flowLayoutDefaultItemSpacing,
+              @ViewBuilder viewMapping: @escaping (Data) -> ItemView) {
+    self.mode = mode
+    _binding = binding
+    self.items = items
+    self.itemSpacing = itemSpacing
+    self.viewMapping = viewMapping
+    _totalHeight = State(initialValue: (mode == .scrollable) ? .zero : .infinity)
+  }
+
+  public var body: some View {
+    let stack = VStack {
+       GeometryReader { geometry in
+         self.content(in: geometry)
+       }
+    }
+    return Group {
+      if mode == .scrollable {
+        stack.frame(height: totalHeight)
+      } else {
+        stack.frame(maxHeight: totalHeight)
+      }
+    }
+  }
+
+  private func content(in g: GeometryProxy) -> some View {
+    var width = CGFloat.zero
+    var height = CGFloat.zero
+    var lastHeight = CGFloat.zero
+    let itemCount = items.count
+    return ZStack(alignment: .topLeading) {
+        ForEach(Array(items.enumerated()), id: \.offset) { index, item in
+            viewMapping(item)
+              .padding([.horizontal, .vertical], itemSpacing)
+              .alignmentGuide(.leading, computeValue: { d in
+                if (abs(width - d.width) > g.size.width) {
+                  width = 0
+                  height -= lastHeight
+                }
+                lastHeight = d.height
+                let result = width
+                if index == itemCount - 1 {
+                  width = 0
+                } else {
+                  width -= d.width
+                }
+                return result
+              })
+              .alignmentGuide(.top, computeValue: { d in
+                let result = height
+                if index == itemCount - 1 {
+                  height = 0
+                }
+                return result
+              })
+        }
+      }
+      .background(HeightReaderView(binding: $totalHeight))
+  }
+
+  public enum Mode {
+    case scrollable, vstack
+  }
+}
+
+private struct HeightPreferenceKey: PreferenceKey {
+  static func reduce(value _: inout CGFloat, nextValue _: () -> CGFloat) {}
+  static var defaultValue: CGFloat = 0
+}
+
+private struct HeightReaderView: View {
+  @Binding var binding: CGFloat
+  var body: some View {
+    GeometryReader { geo in
+      Color.clear
+           .preference(key: HeightPreferenceKey.self, value: geo.frame(in: .local).size.height)
+    }
+    .onPreferenceChange(HeightPreferenceKey.self) { h in
+      binding = h
+    }
+  }
+}
+
+
+public extension FlowLayout where RefreshBinding == Never? {
+    init(mode: Mode,
+         items: [Data],
+         itemSpacing: CGFloat = flowLayoutDefaultItemSpacing,
+         @ViewBuilder viewMapping: @escaping (Data) -> ItemView) {
+        self.init(mode: mode,
+                  binding: .constant(nil),
+                  items: items,
+                  itemSpacing: itemSpacing,
+                  viewMapping: viewMapping)
     }
 }
