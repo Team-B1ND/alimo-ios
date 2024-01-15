acda7811e921fc806c98cb4f8e728212cb312b3b
jyw28 <roy03280328@gmail.com>
2024-01-15 11:12:46 +0900

    feat: Add check pw, pwCheck and show text alert

--- alimo/Projects/App/Sources/UI/Feature/ParentJoinSecondView.swift
+++ alimo/Projects/App/Sources/UI/Feature/ParentJoinSecondView.swift
@@ -12,11 +12,35 @@ struct ParentJoinSecondView: View {
     
     @Environment(\.dismiss) private var dismiss
     
-    @State var id: String = ""
+    @State var email: String = ""
     @State var pw: String = ""
     @State var pwCheck: String = ""
     
+    @State var showTextAlert: Bool = false
+    
     let dummyStudentName: String = "김가영"
+    
+    // 비밀번호랑 비밀번호 재확인에 입력한 문자열이 같은 지 확인하는 함수
+    func isPwcheckSame(pw: String, pwCheck: String) -> Bool {
+        
+        if pw == pwCheck {
+            return true
+        } else {
+            return false
+        }
+        
+    }
+    
+    // 이메일, 비밀번호, 비밀번호 재확인에 문자열을 입력했는지 확인하는 함수
+    func isAllEntered(_ email: String, _ pw: String, _ pwCheck: String) -> Bool {
+        
+        if email != "" && pw != "" && pwCheck != "" {
+            return true
+        } else {
+            return false
+        }
+        
+    }
     
     var body: some View {
         VStack {
@@ -27,11 +51,24 @@ struct ParentJoinSecondView: View {
                 .padding(.top, 30)
                 .padding(.bottom, 10)
             
-            AlimoTextField("이메일", text: $id)
+            AlimoTextField("이메일", text: $email)
             
             AlimoTextField("비밀번호", text: $pw, textFieldType: .password)
             
             AlimoTextField("비밀번호 재입력", text: $pwCheck, textFieldType: .password)
+            
+            if showTextAlert {
+                
+                HStack {
+                    Text("비밀번호가 다릅니다.")
+                        .font(Font.caption)
+                        .foregroundStyle(Color.red500)
+                    Spacer()
+                }
+                .padding(.horizontal, 20)
+                .padding(.top, 5)
+                
+            }
             
             Spacer()
             
@@ -50,23 +87,36 @@ struct ParentJoinSecondView: View {
             }
             .padding(.bottom, 5)
             
-            if id != "" && pw != "" && pwCheck != "" {
-                NavigationLink {
-                    ParentJoinThirdView()
-                } label: {
-                    AlimoButton("다음", buttonType: .yellow) {
-                        print(dummyText)
+            if isAllEntered(email, pw, pwCheck) {
+                
+                if pw == pwCheck {
+                    
+                    NavigationLink {
+                        ParentJoinThirdView()
+                    } label: {
+                        AlimoButton("다음", buttonType: .yellow) {}
+                        .disabled(true)
+                        .padding(.bottom, 30)
                     }
-                    .disabled(true)
+                    
+                } else {
+                    
+                    AlimoButton("다음", buttonType: .yellow) {
+                        showTextAlert = true
+                    }
                     .padding(.bottom, 30)
+                    
                 }
+                
             } else {
-                AlimoButton("다음", buttonType: .none) {
-                    print(dummyText)
-                }
+                AlimoButton("다음", buttonType: .none) {}
                 .disabled(true)
                 .padding(.bottom, 30)
             }
+            
+        }
+        .onAppear() {
+            showTextAlert = false
         }
         .navigationBarBackButtonHidden(true)
         .toolbar {
