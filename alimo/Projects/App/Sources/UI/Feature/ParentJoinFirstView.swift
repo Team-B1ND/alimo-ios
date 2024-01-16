938fb7dc087e4e73f722a4d2790404551b173ba8
jyw28 <roy03280328@gmail.com>
2024-01-17 01:08:28 +0900

    Feat: Add CharacterField

--- alimo/Projects/App/Sources/UI/Feature/ParentJoinFirstView.swift
+++ alimo/Projects/App/Sources/UI/Feature/ParentJoinFirstView.swift
@@ -12,7 +12,7 @@ struct ParentJoinFirstView: View {
     
     @Environment(\.dismiss) private var dismiss
     
-    @State var code: String = ""
+    @State var inputeCode: String = ""
     
     var body: some View {
         VStack {
@@ -23,7 +23,8 @@ struct ParentJoinFirstView: View {
                 .padding(.top, 30)
                 .padding(.bottom, 10)
             
-            AlimoTextField("임시 텍스트 필드", text: $code)
+            SeparatedTextField(length: 6, string: $inputeCode)
+                .padding(.horizontal, 20)
             
             Text("학생 코드가 무엇인가요?")
                 .font(Font.custom(Pretendard.medium.rawValue, size: 12))
@@ -48,7 +49,7 @@ struct ParentJoinFirstView: View {
             }
             .padding(.bottom, 5)
             
-            if code != "" {
+            if inputeCode != "" {
                 NavigationLink {
                     ParentJoinSecondView()
                 } label: {
@@ -84,5 +85,86 @@ struct ParentJoinFirstView: View {
                 }
             }
         }
+    }
+}
+
+struct CharacterField: View {
+    @State var character: String = ""
+    @FocusState var focused: Int?
+    @Binding var characters: [Int : String]
+    
+    var index: Int
+    var onChange: ((_ index: Int, _ char: String) -> Void)
+    
+    var body: some View {
+        TextField(text: $character) {
+            Text("")
+                .font(Font.custom(Pretendard.bold.rawValue, size: 16))
+        }
+        .lineLimit(1)
+        .multilineTextAlignment(.center)
+        .keyboardType(.numberPad)
+        .background(
+            RoundedRectangle(cornerRadius: 8)
+                .foregroundStyle(.white)
+                .frame(height: 52)
+                .clipShape(RoundedRectangle(cornerRadius: Size.large.rawValue))
+                .overlay {
+                    if focused == index {
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.main500, lineWidth: 2)
+                    } else {
+                        if characters[index] != nil {
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.main500, lineWidth: 2)
+                        } else {
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.gray300, lineWidth: 2)
+                        }
+                    }
+                }
+        )
+        .frame(maxWidth: .infinity, alignment: .center)
+        .focused($focused, equals: index)
+        .onChange(of: character) { newValue in
+            onChange(index, character)
+            
+            if newValue.count > 1 {
+                character = String(newValue[newValue.startIndex..<newValue.index(newValue.startIndex, offsetBy: 1)])
+            }
+        }
+        .padding(.horizontal, 3)
+        
+    }
+}
+
+struct SeparatedTextField: View {
+    var length: Int // 입력받은 글자 수
+    @FocusState var focused: Int?
+    // index별로 글자를 저장할 dictionary
+    @State var characters: [Int : String] = [:]
+    
+    @Binding var string: String
+    
+    var body: some View {
+        HStack {
+            ForEach(0..<length) { i in
+                CharacterField(focused: _focused, characters: $characters, index: i) { i, c in
+                    focused = c.isEmpty ? i - 1 : i + 1
+                    characters[i] = c
+                    string = getString()
+                }
+            }
+        }.padding([.vertical], 16)
+    }
+    
+    func getString() -> String {
+        var str = ""
+        for i in 0..<length {
+            if let c = characters[i] {
+                str += c
+            }
+        }
+        return str
     }
 }
