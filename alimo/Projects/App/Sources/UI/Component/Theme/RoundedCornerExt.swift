e21d2b8fe846d345015b9f9b6efe076690cf022e
jyw28 <roy03280328@gmail.com>
2024-01-20 14:59:20 +0900

    refactor: Delete duplicated RoundedCorner

--- alimo/Projects/App/Sources/UI/Component/Theme/RoundedCornerExt.swift
+++ alimo/Projects/App/Sources/UI/Component/Theme/RoundedCornerExt.swift
@@ -14,14 +14,3 @@ extension View {
         clipShape( RoundedCorner(radius: radius, corners: corners) )
     }
 }
-
-struct RoundedCorner: Shape {
-
-    var radius: CGFloat = .infinity
-    var corners: UIRectCorner = .allCorners
-
-    func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-        return Path(path.cgPath)
-    }
-}
