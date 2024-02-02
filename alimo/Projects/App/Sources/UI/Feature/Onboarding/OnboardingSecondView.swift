d90433d1d8022f2e19b268c3344e181652d2d9ca
bswlkh0310 <hhhello0507@gmail.com>
2024-02-02 23:42:03 +0900

    fix : hard code in second view

--- alimo/Projects/App/Sources/UI/Feature/Onboarding/OnboardingSecondView.swift
+++ alimo/Projects/App/Sources/UI/Feature/Onboarding/OnboardingSecondView.swift
@@ -13,76 +13,74 @@ struct OnboardingSecondView: View {
     @State var isClicked: Bool = false
     @State var isAnimating: Bool = false
     
+    @ViewBuilder
+    var snackBar: some View {
+        VStack {
+            Spacer()
+            
+            RoundedCorner(radius: 4)
+                .frame(height: 50)
+                .foregroundStyle(.white)
+                .shadow(radius: 4)
+                .overlay {
+                    HStack {
+                        
+                        Text("세션이 만료되었어요")
+                            .font(.body)
+                        Spacer()
+                        Button {
+                            isClicked = false
+                            isAnimating = false
+                        } label: {
+                            Text("닫기")
+                                .font(.bodyLight)
+                                .foregroundStyle(Color.yellow)
+                        }
+                        
+                    }
+                    .padding(.horizontal, 20)
+                }
+                .padding(.horizontal, 20)
+                .padding(.bottom, 10)
+                .offset(y : isAnimating ? 0 : 100)
+                .animation(.bouncy(duration: 0.5), value: isAnimating)
+            
+        }
+    }
+    
     var body: some View {
         NavigationStack {
             ZStack {
-                
-                VStack {
+                VStack(spacing: 0) {
                     Spacer()
-                    
-                    VStack(alignment: .leading) {
-                        AlimoLogo(type: .yellow)
-                        Text("대소고의 모든 소식")
-                            .font(.subtitle)
-                            .offset(x: 5, y: -10)
+                    HStack {
+                        VStack(alignment: .leading, spacing: 0) {
+                            AlimoLogo(type: .yellow)
+                            Text("대소고의 모든 소식")
+                                .font(.subtitle)
+                                .offset(x: 3, y: -5)
+                        }
+                        Spacer()
                     }
-                    .padding(.trailing, 150)
-                    .padding(.bottom, 80)
-                    
+                    .padding(.leading, 24)
+                    .padding(.bottom, 72)
                     // 스낵바 테스트해 보고 싶어서 이미지를 버튼으로 만들어 놨어요
                     Button {
                         isClicked = true
                         isAnimating = true
                     } label: {
                         Image(Asset.screen)
-                            .padding(.bottom, 100)
                     }
-                    
+                    Spacer()
                     NavigationLink {
                         OnboardingThirdView()
                     } label: {
-                        ZStack {
                             AlimoButton("시작하기", buttonType: .yellow) {}
                                 .padding(.bottom, 30)
                                 .disabled(true)
-                        }
                     }
                 }
-                
-                VStack {
-                    Spacer()
-                    
-                    RoundedCorner(radius: 4)
-                        .frame(height: 50)
-                        .foregroundStyle(.white)
-                        .shadow(radius: 4)
-                        .overlay {
-                            HStack {
-                                
-                                Text("세션이 만료되었어요")
-                                    .font(.body)
-                                
-                                Spacer()
-                                
-                                Button {
-                                    isClicked = false
-                                    isAnimating = false
-                                } label: {
-                                    Text("닫기")
-                                        .font(.bodyLight)
-                                        .foregroundStyle(Color.yellow)
-                                }
-                                
-                            }
-                            .padding(.horizontal, 20)
-                            
-                        }
-                        .padding(.horizontal, 20)
-                        .padding(.bottom, 10)
-                        .offset(y : isAnimating ? 0 : 100)
-                        .animation(.bouncy(duration: 0.5), value: isAnimating)
-                    
-                }
+                snackBar
             }
         }
     }
