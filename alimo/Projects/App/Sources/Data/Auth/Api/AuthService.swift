a6af3ecbc0ff703f945b8c22372fbbc5d3b5c051
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-02-09 21:02:41 +0900

    feat : impl sign in apies

--- alimo/Projects/App/Sources/Data/Auth/Api/AuthService.swift
+++ alimo/Projects/App/Sources/Data/Auth/Api/AuthService.swift
@@ -1,6 +1,23 @@
 import Alamofire
 
+fileprivate let client = AlimoHttpClient.live
+
 final class AuthService {
+    
+    private let path = "/sign-in"
+    
+    func signInTest(_ email: String) async throws -> SignInResponse {
+        try await client.request("\(path)/test?email=\(email)",
+                                 SignInResponse.self,
+                                 method: .post)
+    }
+    
+    func signIn(_ request: SignInRequest) async throws -> SignInResponse {
+        try await client.request("\(path)/",
+                                 SignInResponse.self,
+                                 method: .post, 
+                                 parameters: request)
+    }
     
 }
 
