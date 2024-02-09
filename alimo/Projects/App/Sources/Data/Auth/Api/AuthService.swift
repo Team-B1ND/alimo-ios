0ab6a0005fb8e604a2fbafbe23a399b8b0a2bc56
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-02-09 21:17:36 +0900

    feat : impl auth apies

--- alimo/Projects/App/Sources/Data/Auth/Api/AuthService.swift
+++ alimo/Projects/App/Sources/Data/Auth/Api/AuthService.swift
@@ -4,21 +4,49 @@ fileprivate let client = AlimoHttpClient.live
 
 final class AuthService {
     
-    private let path = "/sign-in"
+    private let signInPath = "/sign-in"
+    private let refreshPath = "/refresh"
+    private let childCodePath = "/verify-childCode"
+    private let signUpPath = "/sign-up"
     
-    func signInTest(_ email: String) async throws -> SignInResponse {
-        try await client.request("\(path)/test?email=\(email)",
-                                 SignInResponse.self,
+    func signInTest(_ email: String) async throws -> ResponseData<TokenResponse> {
+        try await client.request("\(signInPath)/test?email=\(email)",
+                                 ResponseData<TokenResponse>.self,
                                  method: .post)
     }
     
-    func signIn(_ request: SignInRequest) async throws -> SignInResponse {
-        try await client.request("\(path)/",
-                                 SignInResponse.self,
-                                 method: .post, 
+    func signIn(_ request: SignInRequest) async throws -> ResponseData<TokenResponse> {
+        try await client.request("\(signInPath)/",
+                                 ResponseData<TokenResponse>.self,
+                                 method: .post,
                                  parameters: request)
     }
     
+    func signInByDodam(_ request: DodamSignInRequest) async throws -> ResponseData<TokenResponse> {
+        try await client.request("\(signInPath)/dodam", 
+                                 ResponseData<TokenResponse>.self,
+                                 method: .post, parameters: request)
+    }
+    
+    func refresh(_ request: RefreshRequest) async throws -> ResponseData<RefreshResponse> {
+        try await client.request("\(refreshPath)/",
+                                 ResponseData<RefreshResponse>.self,
+                                 method: .post,
+                                 parameters: request)
+    }
+    
+    func verifyChildCode(_ childCode: String) async throws -> ResponseData<ChildCodeResponse> {
+        try await client.request("\(childCode)?child-code=\(childCode)",
+                                 ResponseData<ChildCodeResponse>.self,
+                                 method: .post)
+    }
+    
+    func signUp(_ request: SignUpRequest) async throws -> ResponseData<TokenResponse> {
+        try await client.request("\(signUpPath)/",
+                                 ResponseData<TokenResponse>.self,
+                                 method: .post,
+                                 parameters: request)
+    }
 }
 
 extension AuthService {
