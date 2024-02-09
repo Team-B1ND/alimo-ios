8d7532b00682be8333504331fcae1f10b5423818
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-02-09 21:02:30 +0900

    feat : create sign in response

---
+++ alimo/Projects/App/Sources/Data/Auth/Model/SignInResponse.swift
@@ -0,0 +1,14 @@
+//
+//  SignInResponse.swift
+//  App
+//
+//  Created by dgsw8th71 on 2/9/24.
+//  Copyright © 2024 b8nd. All rights reserved.
+//
+
+import Foundation
+
+struct SignInResponse: Decodable {
+    let accessToken: String
+    let refreshToken: String
+}
