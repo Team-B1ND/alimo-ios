42adcda4e78ac43aec83b0bd62d799c43f59b032
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-02-09 21:17:20 +0900

    feat : create auth request dto

---
+++ alimo/Projects/App/Sources/Data/Auth/Model/Request/RefreshRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RefreshRequest.swift
+//  App
+//
+//  Created by dgsw8th71 on 2/9/24.
+//  Copyright © 2024 b8nd. All rights reserved.
+//
+
+import Foundation
+
+struct RefreshRequest: Encodable {
+    let refreshToken: String
+}
