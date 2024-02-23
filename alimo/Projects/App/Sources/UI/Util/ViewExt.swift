7aea956bdab3314598f4627f4e10ca59f2802105
bswlkh0310 <hhhello0507@gmail.com>
2024-02-23 10:58:20 +0900

    feat : add swipe back

--- alimo/Projects/App/Sources/UI/Util/ViewExt.swift
+++ alimo/Projects/App/Sources/UI/Util/ViewExt.swift
@@ -29,3 +29,14 @@ extension View {
         }
     }
 }
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
