c65e5590bc3c0b062e1a7af8713e8963f6234514
bswlkh0310 <hhhello0507@gmail.com>
2024-02-23 18:49:40 +0900

    feat : create child name member api

---
+++ alimo/Projects/App/Sources/Data/Member/Model/Response/MemberNameResponse.swift
@@ -0,0 +1,13 @@
+//
+//  MemberNameResponse.swift
+//  App
+//
+//  Created by dgsw8th71 on 2/23/24.
+//  Copyright © 2024 b8nd. All rights reserved.
+//
+
+import Foundation
+
+struct MemberNameResponse: Decodable {
+    let name: String
+}
