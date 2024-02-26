5ec0efd49bb640db0849dd7f12faf1d669ad6976
bswlkh0310 <hhhello0507@gmail.com>
2024-02-26 23:10:46 +0900

    fix : notification ceil style

--- alimo/Projects/App/Sources/UI/Feature/Main/Home/Component/Post/NotificationCeil.swift
+++ alimo/Projects/App/Sources/UI/Feature/Main/Home/Component/Post/NotificationCeil.swift
@@ -14,39 +14,55 @@ struct NotificationCeil: View {
     let notification: Notification
     
     @ViewBuilder
-    private var profile: some View {
-        AlimoAvatar().padding(.leading, 12)
+    private var avatar: some View {
+        AlimoAvatar()
             .toTop()
+    }
+    
+    @ViewBuilder
+    private var profile: some View {
+        ProfileCeil(isNew: true, title: notification.title, membername: String(notification.memberId))
+    }
+    
+    @ViewBuilder
+    private var content: some View {
+        Text(notification.content)
+            .lineLimit(6)
+            .font(.caption)
+            .foregroundColor(.main900)
+            .fixedSize(horizontal: false, vertical: true)
+            .multilineTextAlignment(.leading)
+            .lineSpacing(5)
+    }
+    
+    @ViewBuilder
+    private var info: some View {
+        Text(notification.createdAt)
+            .foregroundStyle(Color.gray500)
+            .font(.cute)
+            .padding(.top, 12)
+        IconCeil()
+            .padding(.top, 10)
     }
     
     var body: some View {
         HStack(spacing: 0) {
-            profile
+            avatar
             VStack(alignment: .leading, spacing: 0) {
                 NavigationLink {
                     NotificationDetailView()
                 } label: {
                     VStack(alignment: .leading, spacing: 0) {
-                        ProfileCeil(isNew: true, title: notification.title, membername: String(notification.memberId))
-                        Text(notification.content)
-                            .lineLimit(6)
-                            .font(.caption)
-                            .foregroundColor(.main900)
-                            .fixedSize(horizontal: false, vertical: true)
-                            .multilineTextAlignment(.leading)
-                            .lineSpacing(5)
+                        profile
+                        content
                             .padding(.top, 12)
                     }
                 }
-                Text(notification.createdAt)
-                    .foregroundStyle(Color.gray500)
-                    .font(.cute)
-                    .padding(.top, 12)
-                IconCeil()
-                    .padding(.top, 10)
+                info
             }
             .padding(.leading, 8)
         }
+        .padding(.leading, 12)
         .padding(.top, 20)
         .padding(.trailing, 16)
         .padding(.bottom, 12)
