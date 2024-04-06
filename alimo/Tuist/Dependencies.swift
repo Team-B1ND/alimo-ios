ad3b04e93b0d15b945910e9f476f9f5a6188cdb4
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-04-06 13:10:42 +0900

    build : fix firebase ios sdk version
    10.23.1 -> 10.19.0

--- alimo/Tuist/Dependencies.swift
+++ alimo/Tuist/Dependencies.swift
@@ -11,7 +11,7 @@ let dependencies = Dependencies(
                     requirement: .upToNextMajor(from: "4.3.0")),
             .remote(url: "https://github.com/CSolanaM/SkeletonUI.git", requirement: .branch("master")),
             .remote(url: "https://github.com/apple/swift-crypto.git", requirement: .upToNextMajor(from: "3.0.0")),
-            .remote(url: "https://github.com/firebase/firebase-ios-sdk", requirement: .exact("10.23.1"))
+            .remote(url: "https://github.com/firebase/firebase-ios-sdk", requirement: .exact("10.19.0"))
         ]
     ),
     platforms: [.iOS]
