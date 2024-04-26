a0618c6af749c25f9fc72d8c33b21ec366d3ae54
bswlkh0310 <hhhello0507@gmail.com>
2024-04-26 11:31:40 +0900

    feat : create View+applyAnimation

--- alimo/Projects/App/Sources/UI/Util/ViewExt.swift
+++ alimo/Projects/App/Sources/UI/Util/ViewExt.swift
@@ -8,6 +8,30 @@
 
 import SwiftUI
 
+
+struct AlimoAnimationButton: ButtonStyle {
+    public func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .addPressAnimation(configuration.isPressed)
+    }
+}
+
+public extension View {
+    
+    func addPressAnimation(_ isPressed: Bool) -> some View {
+        self
+            .opacity(isPressed ? 0.64 : 1)
+            .scaleEffect(isPressed ? 0.96 : 1)
+            .animation(.easeOut, value: 1)
+            .disabled(isPressed)
+    }
+    
+    func applyAnimation() -> some View {
+        self
+            .buttonStyle(AlimoAnimationButton())
+    }
+}
+
 extension View {
     func alimoToolbar(_ title: String,
                       onClick: @escaping () -> Void) -> some View {
