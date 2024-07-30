b86cbdaa07d22901c555a298691828c0b5a5aaa8
JoYoungwoo <roy03280328@gmail.com>
2024-07-30 13:35:00 +0900

    feat : Modify OnboardinfFirstView padding

--- Projects/App/Sources/UI/Feature/Onboarding/Onboarding/OnboardingFirstView.swift
+++ Projects/App/Sources/UI/Feature/Onboarding/Onboarding/OnboardingFirstView.swift
@@ -14,39 +14,41 @@ struct OnboardingFirstView: View {
     
     var body: some View {
         NavigationStack {
-            ZStack {
-                VStack(spacing: 0) {
-                    Spacer()
-                    HStack {
-                        VStack(alignment: .leading, spacing: 0) {
-                            AlimoLogo(type: .yellow)
-                            Text("대소고의 모든 소식")
-                                .font(.subtitle)
-                                .offset(x: 3, y: -5)
-                        }
+            GeometryReader { reader in
+                ZStack {
+                    VStack(spacing: 0) {
                         Spacer()
+                        HStack {
+                            VStack(alignment: .leading, spacing: 0) {
+                                AlimoLogo(type: .yellow)
+                                Text("대소고의 모든 소식")
+                                    .font(.subtitle)
+                                    .offset(x: 3, y: -5)
+                            }
+                            Spacer()
+                        }
+                        .padding(.leading, 24)
+                        .padding(.bottom, 30)
+                        // 스낵바 테스트해 보고 싶어서 이미지를 버튼으로 만들어 놨어요
+                        Button {
+                            showEasterEgg = true
+                        } label: {
+                            Image(.screen)
+                        }
+                        .padding(.bottom, reader.size.height / 800 * 123)
+                        NavigationLink {
+                            OnboardingSecondView()
+                        } label: {
+                            AlimoButton("시작하기", buttonType: .yellow) {}
+                                .padding(.bottom, 30)
+                                .disabled(true)
+                        }
                     }
-                    .padding(.leading, 24)
-                    .padding(.bottom, 72)
-                    // 스낵바 테스트해 보고 싶어서 이미지를 버튼으로 만들어 놨어요
-                    Button {
-                        showEasterEgg = true
-                    } label: {
-                        Image(.screen)
+                    .alert(isPresented: $showEasterEgg) {
+                        Alert(title: Text("히히 이걸 찾아니"),
+                              message: Text("대단하시네요"),
+                              dismissButton: .default(Text("닫기")))
                     }
-                    Spacer()
-                    NavigationLink {
-                        OnboardingSecondView()
-                    } label: {
-                        AlimoButton("시작하기", buttonType: .yellow) {}
-                            .padding(.bottom, 30)
-                            .disabled(true)
-                    }
-                }
-                .alert(isPresented: $showEasterEgg) {
-                    Alert(title: Text("히히 이걸 찾아니"),
-                          message: Text("대단하시네요"),
-                          dismissButton: .default(Text("닫기")))
                 }
             }
         }
