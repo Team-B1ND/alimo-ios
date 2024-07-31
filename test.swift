571c30e1fd50caa135988a77ae0ca0c2d7ef9fca
hhhello <hhhello0507@dgsw.hs.kr>
2024-07-31 18:37:02 +0900

    feat: test pr

---
+++ test.swift
@@ -0,0 +1,14 @@
+var number = 10
+number = "Swift"
+
+let a = 10
+let b = 20
+
+if a > b {
+    print("a is greater than b")
+} else {
+    print("a is not greater than b")
+}
+
+let array = [1, 2, 3]
+print(array[3])
